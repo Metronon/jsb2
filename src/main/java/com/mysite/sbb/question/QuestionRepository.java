f2840cd8e9dcc4a6c7768d460206fc96c7370715
Metronon <bumpercar07@gmail.com>
2024-12-19 17:29:16 +0900

    login/logout & list order by id desc

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -14,4 +14,6 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
 	List<Question> findBySubjectLike(String subject);
 
 	Page<Question> findAll(Pageable pageable);
+
+	Page<Question> findAllByOrderByIdDesc(Pageable pageable);
 }
