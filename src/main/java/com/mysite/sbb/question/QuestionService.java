b96160c4fc396a5d3b1d1d53dd845b21bd817b57
Metronon <bumpercar07@gmail.com>
2024-12-28 12:48:40 +0900

    search

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -6,9 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -17,9 +20,11 @@ public class QuestionService {
 
 	private final QuestionRepository questionRepository;
 
-	public Page<Question> getList(int page) {
-		Pageable pageable = PageRequest.of(page, 10);
-		return this.questionRepository.findAllByOrderByIdDesc(pageable);
+	public Page<Question> getList(int page, String kw) {
+		List<Sort.Order> sorts = new ArrayList<>();
+		sorts.add(Sort.Order.desc("createDate"));
+		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+		return this.questionRepository.findAllByKeyword(kw, pageable);
 	}
 
 	public Question getQuestion(Integer id) {
